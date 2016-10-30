ebf5bf42a1de141934369863daf960f5a7eb44a2
KatsuyaTajima <zakichan1216@gmail.com>
2016-10-30 04:58:52 +0000

    add redux

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
-import ReactDOM from "react-dom";
+import { createStore } from 'redux';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        aaaaabbbcc
-      </div>
-    )
-  }
-}
 
-ReactDOM.render(<App />, document.getElementById('content'));
+
+// import React, { Component } from 'react';
+// import ReactDOM from "react-dom";
+//
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         aaaaabbbcc
+//       </div>
+//     )
+//   }
+// }
+//
+// ReactDOM.render(<App />, document.getElementById('content'));
