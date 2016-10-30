3f7d024698dff0e5c9632dd809aab6d677484629
KatsuyaTajima <zakichan1216@gmail.com>
2016-10-30 03:41:56 +0000

    add react code

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-import { createStore } from 'redux';
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        aaaaabbbbb
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('content'));
